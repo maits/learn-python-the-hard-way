496a64d64ed65a0141324614c192bcf479aa03c2
Maite Fernandez <maite.fern@gmail.com>
2014-03-17 23:05:08 -0400

    Create ex28.py

---
+++ ex28.py
@@ -0,0 +1,21 @@
+True and True = true 
+False and True = false 
+1 == 1 and 2 == 1 = true and false 
+"test" == "test" = true 
+1 == 1 or 2 != 1 = true and true = true 
+True and 1 == 1 = true and true = true 
+False and 0 != 0 = false and false = false 
+True or 1 == 1 = true or true = true 
+"test" == "testing" = false 
+1 != 0 and 2 == 1 = true and false = false 
+"test" != "testing" = true 
+"test" == 1 = false 
+not (True and False) = true 
+not (1 == 1 and 0 != 1) = not(true and true) = false 
+not (10 == 1 or 1000 == 1000) = not (false or true) = false 
+not (1 != 10 or 3 == 4) = not (false or false) = false 
+not ("testing" == "testing" and "Zed" == "Cool Guy") = not (true and false) = true 
+1 == 1 and not ("testing" == 1 or 1 == 0) = true and not(false or false) = true 
+"chunky" == "bacon" and not (3 == 4 or 3 == 3) = false and not (false or true) = false 
+3 == 3 and not ("testing" == "testing" or "Python" == "Fun") =  true and not(true or false) = false 
+#i got 11 and 14 wrong
