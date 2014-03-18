e47e8c554444d44427f2fe33087d9d17888217ee
Maite Fernandez <maite.fern@gmail.com>
2014-03-17 21:12:45 -0400

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,14 @@
+formatter = "%r %r %r %r"
+
+print formatter % (1,2,3,4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+	"I had this thing.",
+	"That you could type up right.",
+	"But it didn't sing.",
+	"So I said goodnight."
+)
+
+#mistakes: I put the comma after one in line 4 in the wrong place.
