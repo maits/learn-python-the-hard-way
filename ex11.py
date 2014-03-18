a2d4c100bf2142dcd94fede4c6d70e3fcb0b9307
Maite Fernandez <maite.fern@gmail.com>
2014-03-17 21:13:32 -0400

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+print "What is your favorite color?"
+color = raw_input()
+
+print "So, your favorite color is %r." % (color)
