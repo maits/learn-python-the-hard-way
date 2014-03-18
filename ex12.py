4a73a2276711d9fee03c81f0809d6b398a6d4d3b
Maite Fernandez <maite.fern@gmail.com>
2014-03-17 21:13:55 -0400

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,20 @@
+#from sys import argv
+
+#script, first, second, third = argv
+
+#print "The script is called:", script
+#print "Your first variable is:", first
+#print "Your second variable is:", second
+#print "Your third variable is:", third
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+print "How old are you?",
+age = raw_input()
