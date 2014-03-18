60c30202a4da56ee70d82abf1becfcf555fb9c54
Maite Fernandez <maite.fern@gmail.com>
2014-03-17 21:11:43 -0400

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print "Mary had a little lamb" #prints mary had a little lamb
+print "Its fleece was white as %s." % 'snow' #prints and its fleece..
+print "And everyewhere that Mary went." #prints and everywhere...
+print "." * 10 #what'd that do? "prints 10 periods"
+
+end1 = "C" #stores c in var end1
+end2 = "h" #stores h in var end2
+end3 = "e" #stores e in var end3
+end4 = "e" #stores e in var end4
+end5 = "s" #stores s in var end5
+end6 = "e" #stores e in var end6
+end7 = "B" #stores B in var end7
+end8 = "u" #stores u in var end8
+end9 = "r" #stores r in var end9
+end10 = "g" #stores g in var end10
+end11 = "e" #stores e in var end11
+end12 = "r" #stores r in var end12
+
+#watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, #prints var 1 to 6
+print end7 + end8 + end9 + end10 + end11 + end12 #prints var 7 to 12
