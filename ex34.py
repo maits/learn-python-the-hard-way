44cbeb16f83fa85dcc6c792012904d5ae9926b57
Maite Fernandez <maite.fern@gmail.com>
2014-03-29 18:56:35 -0400

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,32 @@
+#ordinal == ordered, 1st; cardinal == cards at random, 0.
+#Remember if I say "first," "second," etc. then I'm using ordinal, so subtract 1. 
+#If I give you cardinal (0, 1, 2), then use it directly.
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#1. the animal at 1 - cardinal. Directly 
+print animals[1]
+
+#2. The third (3rd) animal. - ordinal. Substract 1
+print animals[2]
+
+#3. The first (1st) animal. - ordinal. Substract 1
+print animals[0]
+
+#4. The animal at 3. - cardinal. directly
+print animals[3]
+
+#5. The The fifth (5th) animal. - ordinal - substract 1
+print animals[4]
+
+#6. The animal at 2. - cardinal. Directly
+print animals[2]
+
+#7. The sixth (6th) animal. - ordinal, substract 1
+print animals[5]
+
+#8. The animal at 4. - cardinal. Directly
+print animals[4]
+
+#to read more about ordinal and cardinal numbers: 
+#http://english.stackexchange.com/questions/28314/whats-the-difference-between-cardinal-and-ordinal-as-adjectives
